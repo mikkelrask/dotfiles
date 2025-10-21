ac24b2ef36cbfe3ae4b030a2048f862ad0af49b9
Mikkel Rask <mikkelrask@protonmail.com>
2025-10-21 09:10:26 +0200

    style: 🎨 mangowc setup with shared wallpaper dir

--- mangowc/.config/mango/autostart.sh
+++ mangowc/.config/mango/autostart.sh
@@ -4,5 +4,5 @@ trayscale --hide-window &
 swaync &
 mpd /home/mr/.config/mpd/mpd.conf &
 sleep 2
-swaybg -i "$WALLPAPER" & disown
+swaybg -i $(cat /home/mr/.local/share/setwall/wallpaper) -m fill & disown
 waybar -c /home/mr/.config/waybar/mango.jsonc -s /home/mr/.config/waybar/mango.css >/dev/null &2>1 
