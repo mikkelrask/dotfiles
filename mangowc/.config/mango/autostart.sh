469394ab4fc81e157fed8fc221cc39745a801c29
Mikkel Rask <mikkelrask@protonmail.com>
2025-12-21 17:56:32 +0100

    ghostty themes

--- mangowc/.config/mango/autostart.sh
+++ mangowc/.config/mango/autostart.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+waybar -c /home/mr/.config/waybar/mango.jsonc -s /home/mr/.config/waybar/mango.css >/dev/null 2>&1 &
+swaybg -i "$(cat /home/mr/.local/share/setwall/wallpaper)" -m fill & disown
 trayscale --hide-window &
 swaync &
 mpd /home/mr/.config/mpd/mpd.conf &
-sleep 2
-swaybg -i $(cat /home/mr/.local/share/setwall/wallpaper) -m fill & disown
-waybar -c /home/mr/.config/waybar/mango.jsonc -s /home/mr/.config/waybar/mango.css >/dev/null &2>1 
+mpdscribble --conf /home/mr/.config/mpdscribble/mpdscribble.conf 
