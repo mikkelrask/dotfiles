21677c976e44f79fa30f8c0365f0cb2888679ba5
Mikkel Rask <mikkelrask@protonmail.com>
2026-04-21 11:14:08 +0200

    utils

---
+++ utils/.local/bin/hyprexit.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+x=$(echo -e "Shutdown\nReboot\nLogout\nLock\nCancel (esc)" | wofi --dmenu -i -p "What to do?")
+case "$x" in
+  Shutdown) shutdown now;;
+  Reboot) reboot;;
+  Logout) hyprctl dispatch exit;;
+  Lock) hyprlock ;;
+  *) echo "[INFO] No action selected. Exiting."
+esac
